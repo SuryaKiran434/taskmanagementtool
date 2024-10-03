7d4138b0407b1b942d67f9ed045c109e2b6da7d5
SuryaKiran434 <suryakiran.katragadda@gmail.com>
2024-10-03 10:48:32 -0400

    Updated Functionality and added logging and fixed errors

--- src/main/java/com/suryakiran/taskmanagementtool/exception/GlobalExceptionHandler.java
+++ src/main/java/com/suryakiran/taskmanagementtool/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.suryakiran.taskmanagementtool.Exception;
+package com.suryakiran.taskmanagementtool.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
