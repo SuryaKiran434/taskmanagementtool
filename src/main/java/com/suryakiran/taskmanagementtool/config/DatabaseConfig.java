6eaf5899c057f4f4cfc2d459a61d4b2a874bd799
SuryaKiran434 <suryakiran.katragadda@gmail.com>
2024-10-07 11:39:27 -0400

    Developing

--- src/main/java/com/suryakiran/taskmanagementtool/config/DatabaseConfig.java
+++ src/main/java/com/suryakiran/taskmanagementtool/config/DatabaseConfig.java
@@ -1,6 +1,5 @@
 package com.suryakiran.taskmanagementtool.config;
 
-import org.hibernate.Hibernate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
