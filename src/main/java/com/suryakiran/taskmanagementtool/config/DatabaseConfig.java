875deee69c862c706b3e6f6cddfca5f0a10aa7ee
SuryaKiran434 <suryakiran.katragadda@gmail.com>
2024-10-09 20:12:50 -0400

    Adding Comments for methods

--- src/main/java/com/suryakiran/taskmanagementtool/config/DatabaseConfig.java
+++ src/main/java/com/suryakiran/taskmanagementtool/config/DatabaseConfig.java
@@ -11,10 +11,11 @@ import org.springframework.transaction.PlatformTransactionManager;
 import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+//Configuration class for database
 @Configuration
 @EnableJpaRepositories(basePackages = "com.suryakiran.taskmanagementtool.repository")
 public class DatabaseConfig {
-
+    // Configures the entity manager factory
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
@@ -24,6 +25,7 @@ public class DatabaseConfig {
         em.setJpaVendorAdapter(vendorAdapter);
         return em;
     }
+    // Configures the transaction manager
 
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
