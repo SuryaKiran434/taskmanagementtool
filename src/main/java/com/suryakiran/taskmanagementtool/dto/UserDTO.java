110b08b8f2bcfcd4e8270d7c6b1807dddf3a7137
SuryaKiran434 <suryakiran.katragadda@gmail.com>
2024-10-16 10:54:23 -0400

    Added Due-Date and missing responses

--- src/main/java/com/suryakiran/taskmanagementtool/dto/UserDTO.java
+++ src/main/java/com/suryakiran/taskmanagementtool/dto/UserDTO.java
@@ -1,7 +1,10 @@
 package com.suryakiran.taskmanagementtool.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 import java.util.List;
 
 @Getter
@@ -13,4 +16,6 @@ public class UserDTO {
     private String email;
     private String password;
     private List<String> roles;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy")
+    private Date createdAt;
 }
